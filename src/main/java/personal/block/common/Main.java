7a9e8b98a39310abb79e484b4f6f62ec5c2ae2ac
yaoqj <yaoqj@terminus.io>
2015-04-25 00:20:38 +0800

    test

---
+++ src/main/java/personal/block/common/Main.java
@@ -0,0 +1,10 @@
+package personal.block.common;
+
+/**
+ * Created by yaoqijun on 25/4/15.
+ */
+public class Main {
+    public static  void main(String []args){
+        System.out.print("hello world");
+    }
+}
